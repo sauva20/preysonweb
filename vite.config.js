27c1961955dea551b70298ae37bdf56b8c4c195b
sauva20 <2qistysauva@gmail.com>
2026-07-25 12:47:10 +0700

    fix: disable LightningCSS to resolve @keyframes build error on hosting

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    transformer: 'postcss',
+    devSourcemap: false,
+  },
+  build: {
+    cssMinify: false,
+  },
 })
+
+
